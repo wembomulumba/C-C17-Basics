9b8b407e2a1a13568ae0409d2196e0425a7ca18d
Wembo Mulumba <fabriceotepa@hotmail.com>
2019-02-23 21:42:38 +0200

    Update Pointer001.cpp
    code reviews

--- Pointer001.cpp
+++ Pointer001.cpp
@@ -4,24 +4,14 @@
 #include <stdio.h>
 #include<sys/posix_sem.h>
 #include <sys/posix_shm.h>
-
-
 using namespace std;
 
 int main() {
-
    int x = 25;
-
    int y = 5;
-
    int* q = &y;
    int* p = &x;
-
-   
-  
-
    cout << p  << ' ' << x <<endl;
    cout << q  << ' '  << y + *p <<endl;
-
     return 0;
 }
